c64bf136e0ff65d37baa2d68ca1f4219708a8744
LessEase <chshaoyi7@gmail.com>
2016-03-30 11:05:21 +0800

    generate online

--- feature_p/chenshaoyi/generate_intersact_features.sh
+++ feature_p/chenshaoyi/generate_intersact_features.sh
@@ -7,5 +7,7 @@ root_dir=../..
 # out_feature: ../
 
 
-python get_word_intersaction.py ../basic_users_features ../basic_items_features ${root_dir}/${local_samples}/local_samples.csv ../local/local_train_features.p 
-python get_word_intersaction.py ../basic_users_features ../basic_items_features ${root_dir}/${local_samples}/local_test_samples.csv ../local/local_test_features.p 
+#python get_word_intersaction.py ../basic_users_features ../basic_items_features ${root_dir}/${local_samples}/local_samples.csv ../local/local_train_features.p 
+#python get_word_intersaction.py ../basic_users_features ../basic_items_features ${root_dir}/${local_samples}/local_test_samples.csv ../local/local_test_features.p 
+python get_word_intersaction.py ../basic_users_features ../basic_items_features ${root_dir}/${online_samples}/online_samples.csv ../online/online_train_features.p 
+python get_word_intersaction.py ../basic_users_features ../basic_items_features ${root_dir}/${online_samples}/online_test_samples.csv ../online/online_test_features.p 
